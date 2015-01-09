6698fae6b80cb0481f24c93562d1a7e0057e2ee2
Travis Huber <thuber@maxdigital.com>
2015-01-09 17:44:20 +0000

    robustify setup script

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Use vim as my personal python ide
 An easy way to use this would be
 ```
 git clone https://github.com/thuber-max/vim-ide.git ~/.vim && \
-bash ~/.vim/setupSubmodules.sh
+cd ~/.vim && \
+bash ~/.vim/setup_vim.sh
 ```
 
 That should be all there is to it.
--- setup_vim.sh
+++ setup_vim.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+echo -e "\nConfiguring vim\n"
+
+if [ ! -e "~/.vim/autoload" ] || [ ! -e "~/.vim/bundle"]; then
+    mkdir ~/.vim/{autoload,bundle}
+fi
+
 git submodule add https://github.com/tpope/vim-pathogen bundle/vim-pathogen
 git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
 git submodule add https://github.com/msanders/snipmate.vim.git bundle/snipmate
@@ -8,7 +14,7 @@ git submodule add https://github.com/tpope/vim-git.git bundle/git
 git submodule add https://github.com/ervandew/supertab.git bundle/supertab
 git submodule add https://github.com/fholgado/minibufexpl.vim bundle/minibufexpl
 git submodule add https://github.com/wincent/Command-T.git bundle/command-t
-git submodule add https://github.com/mitechie/pyflakes-pathogen.git
+git submodule add https://github.com/mitechie/pyflakes-pathogen.git bundle/pyflakes-pathogen
 git submodule add https://github.com/mileszs/ack.vim.git bundle/ack
 git submodule add https://github.com/sjl/gundo.vim.git bundle/gundo
 git submodule add https://github.com/fs111/pydoc.vim.git bundle/pydoc
@@ -23,5 +29,31 @@ git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
 
-ln -s ~/.vim/vimrc ~/.vimrc
+
+file=~/.vimrc
+echo -e "\n\nSubmodules configured.\nTime to setup $file\n\n"
+#   various tests for file existence
+#   [ -f $file ] && [ ! -L $file ] && echo "$file exists and is not a symlink"
+#   [ -L $file ] && echo "$file is a symlink and exists"
+#   [ -h $file ] && echo "$file is a symlink"
+#   [ -e $file ] && echo "$file exists"
+
+
+if [ -f $file ]; then
+    echo "$file already exists"
+
+    if grep -q "runtime bundle/vim-pathogen/autoload/pathogen.vim" "$file"; 
+    then
+        echo "$file already configured for pathogen"
+    else
+        echo "Adding pathogen config to $file"
+        echo "runtime bundle/vim-pathogen/autoload/pathogen.vim" >> "$file"
+    fi
+else
+    echo "$file does not exist -> linking to the default"
+    ln -s ~/.vim/vimrc "$file"
+fi
+
+echo -e "\nvim configured\n"
+
 
